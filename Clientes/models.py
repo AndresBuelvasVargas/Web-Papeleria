6b5a43080452aefc3018232426494784be0d0d17
AndresBuelvasVargas <andresfbuelvasvargas@gmail.com>
2025-08-29 18:19:24 -0500

    Se conecta a la base de datos de MariaDB mysql

--- Clientes/models.py
+++ Clientes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    ClienteID = models.AutoField(primary_key=True)
+    Nombre = models.CharField(max_length=100, null=False)
+    Direccion = models.CharField(max_length=200, null=False)
+    Telefono = models.CharField(max_length=15, unique=True, null=False) 
+    Email = models.EmailField(unique=True, null=False)
+
+def __str__(self):
+    return self.Nombre
--- Web_papeleria_API/settings.py
+++ Web_papeleria_API/settings.py
@@ -37,12 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'Clientes',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -75,9 +78,13 @@ WSGI_APPLICATION = 'Web_papeleria_API.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "Web_papeleria",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
@@ -122,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS configuration
+CORS_ALLOWED_ORIGINS = []
